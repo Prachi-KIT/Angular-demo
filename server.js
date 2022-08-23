d7c4eaa76033b669d2349f97ed25c8b68d4fd55b
GuptaRinkal <gprachi251@gmail.com>
2022-08-23 19:09:35 +0530

    Some more changes

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files from the dist directory 
-app.use(express.static('../../assets'));
+//app.use(express.static('../../assets'));
+app.use(express.static('./src/assets/img'))
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: '../src'}),
+    res.sendFile('index.html', {root: './src/dist/Shopping-and-Receipe'}),
 );
 
 // Start the app by listening on the default Heroku port
